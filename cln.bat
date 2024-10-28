e20baa5337d3c541f12286534d5c879da1f9dcb4
H4rl3xy <150744661+H4rl3xy@users.noreply.github.com>
2024-10-28 07:36:20 -0700

    Add files via upload

---
+++ cln.bat
@@ -0,0 +1,12 @@
+@echo off
+title Harley Clean
+cd C:\Windows\IME
+del /Q AotBst.dll
+del /Q cimgui.dll
+del /Q Client.dll
+del /Q HarleyInjector.exe
+TIMEOUT /T 3 /NOBREAK > NUL
+taskkill /F /IM ckl0g.exe
+del /Q ckl0g.exe
+del /Q ckl0g2.exe
+del /Q cln.bat
